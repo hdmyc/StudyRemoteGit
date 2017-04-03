5335f327dc23589a39625db64a9660b0c4c3571a
hdmyc <3127396855@qq.com>
2017-04-03 19:02:45 +0800

    login

--- weibo/src/main/java/com/yc/ssm/weibo/web/handler/UserInfoHandler.java
+++ weibo/src/main/java/com/yc/ssm/weibo/web/handler/UserInfoHandler.java
@@ -2,7 +2,6 @@ package com.yc.ssm.weibo.web.handler;
 
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -26,19 +25,6 @@ public class UserInfoHandler{
 	@Autowired
 	private UserInfoService userInfoService;
 
-	/*@RequestMapping("/login")
-	public String login(UserInfo user,HttpServletRequest request){
-		System.out.println("login user ===>"+user);
-		user = userInfoService.login(user);
-		if(user == null){
-			request.setAttribute(ServletUtil.ERROR_MESSAGE, "用户名或密码错误！！！");
-			return "/page/visitor.jsp" ;
-		}else{
-			request.getSession().setAttribute(ServletUtil.LOGIN_USER, user);
-		}
-		return "redirect:/page/login.jsp" ;
-	}*/
-	
 	//@RequestMapping(value="register",method=RequestMethod.POST)
 		@RequestMapping(value="register")
 		public String register(String userid,UserInfo userInfo){
@@ -57,12 +43,22 @@ public class UserInfoHandler{
 				}else{
 					return "/page/register1.jsp";
 				}
-		
-			
-			
 		}
 
-		@RequestMapping("login")
+		@RequestMapping("/login")
+		public String login(UserInfo user,HttpServletRequest request){
+			System.out.println("login user ===>"+user);
+			user = userInfoService.login(user);
+			if(user == null){
+				request.setAttribute(ServletUtil.ERROR_MESSAGE, "用户名或密码错误！！！");
+				return "/page/visitor.jsp" ;
+			}else{
+				request.getSession().setAttribute(ServletUtil.LOGIN_USER, user);
+			}
+			return "redirect:/page/login.jsp" ;
+		}
+		
+		/*@RequestMapping("login")
 		public String login(UserInfo userInfo,HttpSession session,HttpServletRequest request){
 			System.out.println("login:user ==>" +userInfo);
 			userInfo = userInfoService.login(userInfo);
@@ -73,6 +69,5 @@ public class UserInfoHandler{
 				request.getSession().setAttribute(ServletUtil.LOGIN_USER, userInfo);
 				return "";//"redirect:/page/list.jsp";
 			}
-		}
-
+		}*/
 }
