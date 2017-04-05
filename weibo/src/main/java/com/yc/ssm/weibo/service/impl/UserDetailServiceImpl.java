d0073fa931aa362de1d6cc37088090b1cb796bc0
hdmyc <3127396855@qq.com>
2017-04-05 22:06:40 +0800

    fs

---
+++ weibo/src/main/java/com/yc/ssm/weibo/service/impl/UserDetailServiceImpl.java
@@ -0,0 +1,28 @@
+package com.yc.ssm.weibo.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yc.ssm.weibo.entity.UserDetail;
+import com.yc.ssm.weibo.mapper.UserDetailMapper;
+import com.yc.ssm.weibo.service.UserDetailService;
+
+@Service("userDetailService")
+public class UserDetailServiceImpl implements UserDetailService {
+	
+	@Autowired		
+	private UserDetailMapper userDetailMapper;
+
+	@Override
+	public boolean modifyUsers(UserDetail userDetail) {
+		System.out.println("修改之前====>"+userDetail);
+		return userDetailMapper.updateDetail(userDetail)>0;
+	}
+
+	@Override
+	public UserDetail findDetail(UserDetail userDetail) {
+		System.out.println("登录之后===》"+userDetail);
+		return userDetailMapper.findDetail(userDetail);
+	}
+
+}
