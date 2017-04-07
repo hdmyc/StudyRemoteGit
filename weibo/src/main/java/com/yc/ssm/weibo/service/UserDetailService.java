03cbcf7c628ffad7f737567f7da6c0438c3769af
hdmyc <3127396855@qq.com>
2017-04-07 19:19:19 +0800

    detail

--- weibo/src/main/java/com/yc/ssm/weibo/service/UserDetailService.java
+++ weibo/src/main/java/com/yc/ssm/weibo/service/UserDetailService.java
@@ -3,14 +3,12 @@ package com.yc.ssm.weibo.service;
 
 import com.yc.ssm.weibo.entity.UserDetail;
 
-import java.util.List;
-
 
 public interface UserDetailService {
 
 	boolean modifyUsers(UserDetail userDetail);
 
-	public List<UserDetail> findAll();
+	//public List<UserDetail> findAll();
 	
 	UserDetail findDetail(UserDetail userDetail);
 
