8e3f3b7a090e551a838f772cba2ac46dfbec99fa
hdmyc <3127396855@qq.com>
2017-04-07 19:18:48 +0800

    detail

--- weibo/src/main/webapp/js/detail.js
+++ weibo/src/main/webapp/js/detail.js
@@ -1,13 +1,41 @@
-//添加用户详情
+//用户详情
 alert(1);
 $("#detail").form({
 	url:"detail/findDetail",
-	
+	onSubmit: function(){    
+        alert(1)  
+    },    
+    success:function(data){    
+        alert(data)    
+    }
 });
+$('#detail').submit(); 
 
+$('#birthString').datebox({    
+    required:true   
+}); 
+
+//取到用户信息
+loaduserDetail();
+function loaduserDetail(){
+	//以异步的方式取到所有书签的信息
+	$.get("detail", function(data){
+		//alert("请求响应成功..."+data);
+		//alert(JSON.stringify(data));  //JSON.stringify() ,把json对象转换成json字符串
+		var userDeailStr = "";
+		for (var i = 0; i < data.rows.length; i++) {
+			userDeailStr+='<p><label>'+data.rows[i].userid+'</label>'
+			userDeailStr+='<input type="text" name="userid" id="userid"'+data.rows[i].userid+'/></p>';
+			comments(data.rows[i].userDetailId);
+		}
+		$("#detailDiv")[0].innerHTML = userDeailStr;
+		$("#detail put").hover(selecrLabel);
+	}, "json");
+}
+
+openDetail();
 function openDetail(userid){
 	var userid = $("#detail").datagrid("getUserid")[userid];
-	$("#detailDiv").dialog("open");
 	$("#nickname").html(userid.nickname);
 	$("#username").html(userid.username);
 	$("#birthString").html(userid.birthString);
